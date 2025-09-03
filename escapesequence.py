d43b67b854a5921c64697f92ceea1c69d73286da
swarupa kumari <swarupakumari126@gmail.com>
2025-09-03 10:48:55 +0530

    escape sequence

---
+++ escapesequence.py
@@ -0,0 +1,12 @@
+#for comment use #
+#for new lin \n
+print("hello guys \n How are you !!")
+# for comment '''   ''' , """    """==> multine comment, #
+# ----------------------------------------------------------------------------------------------
+print("hello \"guys\" How are you !!")#so if we want to add "" in printing we have to use escape sequence as \"
+
+
+#------------------------------------------------
+# sepertator ------> it will seperate with ~  and end will also after other print before end kr deta hai 
+print ("Hey",6,7,sep="~",end="009\n")
+print("Swarupa")
