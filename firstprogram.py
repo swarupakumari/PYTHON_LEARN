6e7b1cf406050684eb243b3439c887b507cd200b
swarupa kumari <swarupakumari126@gmail.com>
2025-09-03 10:27:25 +0530

    first program

---
+++ firstprogram.py
@@ -0,0 +1,2 @@
+print("Hello World")  #print fxn is to print and also should writ inside " " only 
+print(5*10)
---
+++ module.py
@@ -0,0 +1,9 @@
+#module is of two type 
+# built in and pre defined modules
+# likes pandas,skitlearn etc
+#using python -m pip install pandas
+# shows to install it
+print('hi')
+import pandas as pd
+
+print("Pandas version:", pd.__version__)
