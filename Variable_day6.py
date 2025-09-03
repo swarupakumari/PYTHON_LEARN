abf75b115371065960ddaa53ab9ec58bc6189a13
swarupa kumari <swarupakumari126@gmail.com>
2025-09-03 11:43:06 +0530

    operator

---
+++ Variable_day6.py
@@ -0,0 +1,9 @@
+# some store data is called variable
+# for knowing which data type it can v asekd by type(a)
+a=10
+print("Type of A is ",type(a))
+#datatypes are 1. int ,float,complex,string,boolean,list ,tuple,disctionary
+#mutable--> mutations --> chnge ho jata hai n immutable opposite
+# everything in python is object 
+
+
