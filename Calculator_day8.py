7c0850f8aabdf4ee38537473dd2aa660c8c0155e
swarupa kumari <swarupakumari126@gmail.com>
2025-09-03 11:53:06 +0530

    calculator

---
+++ Calculator_day8.py
@@ -0,0 +1,7 @@
+a=50
+b=10
+print("The value of",a,"+",b,"is :",a+b)
+print("The value of",a,"-",b,"is :",a-b)
+print("The value of",a,"*",b,"is :",a*b)
+print("The value of",a,"/",b,"is :",a/b)
+print("The value of",a,"//",b,"is :",a//b)
