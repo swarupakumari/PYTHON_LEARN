4732e042810dc2100015784cac47b21597e94f38
swarupa kumari <swarupakumari126@gmail.com>
2025-09-09 21:55:57 +0530

    is ==

---
+++ is_equal_.py
@@ -0,0 +1,27 @@
+# 🔹 == (Equality Operator)
+
+# Checks values of two objects.
+
+# Returns True if the contents are the same, even if they are different objects in memory.
+
+# Example:
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+
+# print(a == b)   # True → because both lists have same contents
+
+# 🔹 is (Identity Operator)
+
+# Checks whether two variables point to the same object in memory (same reference).
+
+# Returns True only if both variables refer to the exact same object.
+
+# Example:
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# c = a
+
+# print(a is b)   # False → different objects in memory
+# print(a is c)   # True → both point to the same object
